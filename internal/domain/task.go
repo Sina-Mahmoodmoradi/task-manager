ac1e07f59ed4e1df477a5e1e53d7f8b232cef83d
Sina Mahmudmoradi <sina.mahmudmoradi@gmail.com>
2025-08-05 15:28:41 +0330

    add TaskService interface

--- internal/domain/task.go
+++ internal/domain/task.go
@@ -13,3 +13,13 @@ type TaskRepository interface {
     Create(task *Task) error
     GetByID(id uint) (*Task, error)
 }
+
+
+
+type TaskService interface {
+    CreateTask(userID uint, title, description string) (*Task, error)
+    GetTaskByID(taskID uint) (*Task, error)
+    UpdateTask(taskID uint, title, description string) (*Task, error)
+    DeleteTask(taskID uint) error
+    ListTasksByUser(userID uint) ([]Task, error)
+}
