601d41de2dc93b756726a110d270ac4ecf30c0e9
Sina Mahmudmoradi <sina.mahmudmoradi@gmail.com>
2025-08-06 11:57:51 +0330

    add setup router

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,15 @@ import (
     "log"
 
     "github.com/Sina-Mahmoodmoradi/task-manager/internal/router"
-	"github.com/Sina-Mahmoodmoradi/task-manager/internal/config"
+	"github.com/Sina-Mahmoodmoradi/task-manager/pkg/database"
 )
 
 func main() {
-    config.InitDB()
-    r := router.InitRouter()
+    db, err := database.NewDatabase()
+    if err != nil {
+        log.Fatalf("could not initialize database: %v", err)
+    }
+    r := router.SetupRouter(db)
 
     log.Println("🚀 Server running at http://localhost:8080")
     if err := r.Run(":8080"); err != nil {
--- internal/router/router.go
+++ internal/router/router.go
@@ -1,14 +1,34 @@
 package router
 
 import (
-    "github.com/gin-gonic/gin"
-    "github.com/Sina-Mahmoodmoradi/task-manager/internal/handler"
+	"github.com/gin-gonic/gin"
+
+	"github.com/Sina-Mahmoodmoradi/task-manager/internal/handler"
+	"github.com/Sina-Mahmoodmoradi/task-manager/internal/service"
+	"github.com/Sina-Mahmoodmoradi/task-manager/internal/repository"
+	"github.com/Sina-Mahmoodmoradi/task-manager/pkg/database"
 )
 
-func InitRouter() *gin.Engine {
+
+func SetupRouter(db *database.Database) *gin.Engine {
     r := gin.Default()
+    
+	// Repositories
+	userRepo := repository.NewUserRepository(db.DB)
 
-    r.GET("/ping", handler.PingHandler)
+	// Services
+	userService := service.NewUserService(userRepo)
 
-    return r
+	// Handlers
+	userHandler := handler.NewUserHandler(userService)
+    
+	// Routes
+	api := r.Group("/api/v1")
+	{
+		api.GET("/ping", handler.PingHandler)
+		api.POST("/register", userHandler.Register)
+		api.POST("/login", userHandler.Login)
+	}
+
+	return r
 }
