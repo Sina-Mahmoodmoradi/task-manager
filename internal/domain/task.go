b8b1a0fee892601c5f54bede6700b7e4282a4711
Sina Mahmudmoradi <sina.mahmudmoradi@gmail.com>
2025-08-09 15:07:40 +0330

    add update to task service and repository interface and add TaskUpdate struct

--- internal/domain/task.go
+++ internal/domain/task.go
@@ -11,17 +11,22 @@ type Task struct {
 
 type TaskRepository interface {
     Create(task *Task) error
+    Update(task *Task) error
     GetByID(id uint) (*Task, error)
     Delete(id uint) error
     GetByUserId(userID uint) ([]Task, error)
 }
 
-
+type TaskUpdate struct {
+    Title       *string
+    Description *string
+    Done        *bool
+}
 
 type TaskService interface {
     CreateTask(userID uint, title, description string) (*Task, error)
     GetTaskByID(taskID uint) (*Task, error)
-    // UpdateTask(taskID uint, title, description string) (*Task, error)
+    UpdateTask(taskID uint, updates TaskUpdate) (*Task, error)
     DeleteTask(taskID uint) error
     ListTasksByUser(userID uint) ([]Task, error)
 }
